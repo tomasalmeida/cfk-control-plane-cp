2f6c86f17027089eda64fe77647f4b24101ac0f2
Tomás Dias Almeida <tomas.almeida@gmail.com>
2023-07-04 23:13:27 +0200

    scripts

--- complex-example/start-cp-demo-common.sh
+++
@@ -1,7 +0,0 @@
-rm -rf cp-demo
-git clone https://github.com/confluentinc/cp-demo
-echo "✔︎ cp-demo cloned"
-
-cd cp-demo
-git checkout 7.4.0-post
-echo "✔︎ CP 7.4.0 defined"
---
+++ complex-example/start-cp-demo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+OS=`uname -s`
+
+if  ([ "$OS" != "Linux" ] || [grep -q "host.internal.docker" /etc/hosts])
+then
+    echo "✔︎ no need to update /etc/hosts"
+else
+    echo "You may be asked to add a password (sudo command executed)"
+    sudo sh -c 'echo "$(dig +short `hostname` | head -n1) host.docker.internal"  >> /etc/hosts'
+    echo "✔︎ /etc/hosts updated"
+fi
+
+# rm -rf cp-demo
+# git clone https://github.com/confluentinc/cp-demo
+# echo "✔︎ cp-demo cloned"
+
+# cd cp-demo
+# git checkout 7.4.0-post
+# echo "✔︎ CP 7.4.0 defined"
