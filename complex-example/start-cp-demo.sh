8badfc0bd8d1bb127f6c149911d9fd80f52749c5
Tomás Dias Almeida <tomas.almeida@gmail.com>
2023-07-05 21:54:57 +0200

    scripts

--- complex-example/start-cp-demo.sh
+++ complex-example/start-cp-demo.sh
@@ -11,10 +11,22 @@ else
     echo "✔︎ /etc/hosts updated"
 fi
 
-# rm -rf cp-demo
-# git clone https://github.com/confluentinc/cp-demo
-# echo "✔︎ cp-demo cloned"
+rm -rf cp-demo
+git clone https://github.com/confluentinc/cp-demo
+echo "✔︎ cp-demo cloned"
 
-# cd cp-demo
-# git checkout 7.4.0-post
-# echo "✔︎ CP 7.4.0 defined"
+cd cp-demo
+git checkout 7.4.0-post
+echo "✔︎ CP 7.4.0 defined"
+
+if  [ "$OS" == "Linux" ]
+then
+    sed -i 's/,dns:localhost/,dns:localhost,dns:host.docker.internal/g' scripts/security/certs-create-per-user.sh
+    sed -i 's/"DNS.4 = kafka2"/"DNS.4 = kafka2" "DNS.5 = host.docker.internal"/g' scripts/security/certs-create-per-user.sh
+else
+    sed -i '' 's/,dns:localhost/,dns:localhost,dns:host.docker.internal/g' scripts/security/certs-create-per-user.sh
+    sed -i '' 's/"DNS.4 = kafka2"/"DNS.4 = kafka2" "DNS.5 = host.docker.internal"/g' scripts/security/certs-create-per-user.sh
+fi
+
+export CLEAN="true" && export C3_KSQLDB_HTTPS="false" && export VIZ="false" && ./scripts/start.sh
+cd ..
